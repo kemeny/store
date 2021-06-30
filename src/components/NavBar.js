0b666d739af58817849a08e3935a7b3bde0ee8fe
kemeny <francisco@kemeny.cl>
2021-06-29 22:34:20 -0400

    added category listings to nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,18 @@
 import react from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 export default function NavBar() {
     return(
         <Navbar className="topnav" fixed="top">
             <Navbar.Brand className="justify-content-left">K-Store</Navbar.Brand>
         <Nav>
-            <a href="/">Store</a>
+            <NavDropdown title="Categorías">
+                <NavDropdown.Item href="#category1">Beers</NavDropdown.Item>
+                <NavDropdown.Item href="#category2">Ducks</NavDropdown.Item>
+                <NavDropdown.Item href="#category3">Stars</NavDropdown.Item>
+            </NavDropdown>
             <a href="/components/pages/Products.js">Productos</a>
             <a href="/components/pages/Cart.js">Cart</a>
         </Nav>
